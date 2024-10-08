0c692a619758e1d4e480a45f0f38e89630bd5279
eliasug98 <eliasug98@hotmail.com>
2024-10-08 02:20:02 -0300

    update3

--- FurnitureStoreProject.API/DTOs/ProductDTOs/ProductDto.cs
+++ FurnitureStoreProject.API/DTOs/ProductDTOs/ProductDto.cs
@@ -2,6 +2,8 @@
 {
     public class ProductDto
     {
+        public int Id { get; set; }
+
         public string Image { get; set; }
 
         public bool Available { get; set; }
@@ -11,5 +13,7 @@
         public string Description { get; set; }
 
         public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
     }
 }
