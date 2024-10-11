bbba9d443eb5c10c9f27d3baf6c62f2c53d881e5
eliasug98 <eliasug98@hotmail.com>
2024-10-11 07:30:19 -0300

    update3

--- FurnitureStoreProject.API/DTOs/UserDTOs/UserLoginDto.cs
+++ FurnitureStoreProject.API/DTOs/UserDTOs/UserLoginDto.cs
@@ -9,7 +9,6 @@ namespace FurnitureStore.API.DTOs.UserDTOs
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password is required.")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; } = string.Empty;
     }
 }
