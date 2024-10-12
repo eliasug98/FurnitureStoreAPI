6322cd93f2641ad01efdc6dda4c435c41f2b6ed9
eliasug98 <eliasug98@hotmail.com>
2024-10-12 00:22:12 -0300

    update3

--- FurnitureStoreProject.API/DTOs/UserDTOs/UsersDto.cs
+++ FurnitureStoreProject.API/DTOs/UserDTOs/UsersDto.cs
@@ -4,6 +4,7 @@ namespace FurnitureStore.API.DTOs.UserDTOs
 {
     public class UsersDto
     {
+        public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
